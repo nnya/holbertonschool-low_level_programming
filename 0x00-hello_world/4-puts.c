51a2557d284a78753e9255582bf5cb80ff48335e
Nestor Nana <makak747@hotmail.com>
2018-06-21 01:24:19 +0000

    capitalized the R in return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - starting point for programming
- * return: value
+ *
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
