d5d9a617907237c2b2f1bcfb904ea4399ba951ac
Nestor Nana <makak747@hotmail.com>
2018-06-21 02:40:52 +0000

    printf of sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - program that prints size
+*Return: 0
+*/
+int main(0)
+{
+printf("Size of a char: %d byte(s)", sizeof(char));
+printf("Size of an int: %d byte(s)", sizeof(int));
+printf("Size of a long int: %d bytes(s)", sizeof(long int));
+printf("Size of a long long int: %d bytes", sizeof(long long int));
+printf("Size of a float: %d bytes", sizeof(float int));
+return (0);
+}
