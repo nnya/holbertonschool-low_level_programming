6798ea6c1db6aa4b583ef2aa741cb7ef627a41f5
Nestor Nana <makak747@hotmail.com>
2018-06-21 23:54:39 +0000

    removed some space for betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,19 +14,18 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-
 if (n > 0)
 {
-printf("is positive\n");
+printf("%i is positive\n", n);
 }
 if (n == 0)
 {
-printf("is zero\n");
+printf("%i is zero\n", n);
 }
 
 if (n < 0)
 {
-printf("is negative\n");
+printf("%i is negative\n", n);
 }
 return (0);
 }
