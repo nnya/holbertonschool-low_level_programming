f10b315bfb43ad977a9efa9f93615d992ed10da3
Nestor Nana <makak747@hotmail.com>
2018-06-21 01:18:09 +0000

    adjusment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
-* main - starting point for programming
-* return: value
-*/
+/**
+ * main - starting point for programming
+ * return: value
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
