c4477fb4de8277be7e0fc36921a48f5b808d0881
Nestor Nana <makak747@hotmail.com>
2018-06-22 01:53:21 +0000

    adjusted space

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - start
+*Return: 0
+*/
+
+int main(void);
+
+{
+int n;
+
+for (n = 97; n <= 122 ; n++)
+
+{
+if (n != 101 && n != 113)
+{
+putchar(n);
+}
+putchar(10);
+return (0);
+}
+}
