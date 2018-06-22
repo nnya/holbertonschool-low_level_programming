9643b357cea87373891933e2535a75e0e7f60198
Nestor Nana <makak747@hotmail.com>
2018-06-22 05:07:33 +0000

    printing lower cases in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - start
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
