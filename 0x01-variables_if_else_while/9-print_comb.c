1b79fb5e86668d47ec6c49d90bb21372e15e2757
Nestor Nana <makak747@hotmail.com>
2018-06-22 07:01:56 +0000

    update code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@ int main(void)
 	int n;
 
 	for (n = '0'; n <= '9'; n++)
-		putchar(n);
 	{
+		putchar(n);
 		if (n < '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 		putchar('\n');
 	return (0);
 }
