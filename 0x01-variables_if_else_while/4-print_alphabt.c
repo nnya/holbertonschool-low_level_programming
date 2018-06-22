1019a541480a25e18f69bc51e954793b4e6bb429
Nestor Nana <makak747@hotmail.com>
2018-06-22 01:54:36 +0000

    adjusted spece

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,17 @@
 *Return: 0
 */
 
-int main(void);
-
+int main(void)
 {
 int n;
 
 for (n = 97; n <= 122 ; n++)
-
 {
 if (n != 101 && n != 113)
 {
 putchar(n);
 }
+}
 putchar(10);
 return (0);
-}
 }
