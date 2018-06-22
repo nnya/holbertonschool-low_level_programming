d8431d016cbec159e3e13b290cabcabe51d1e269
Nestor Nana <makak747@hotmail.com>
2018-06-22 02:33:17 +0000

    aplhabet in upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - start
+* Return: 0
+*/
+
+int main(void)
+{
+int n;
+for (n = 97; n <= 122; n++)
+{
+putchar(n)
+}
+for (n = 65; n <= 90; n++)
+{
+putchar(n)
+}
+putchar(10);
+return (0);
+}
