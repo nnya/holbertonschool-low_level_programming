96243789e9ef772465efa05b169ff88ce6694b10
Nestor Nana <makak747@hotmail.com>
2018-06-22 06:59:40 +0000

    Random commit message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	{
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	return (0);
+}
