0bce220241604ad9d896e9f1c4281bb54fcb585f
Nestor Nana <makak747@hotmail.com>
2018-06-25 22:54:11 +0000

    changed printf to _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- *main - start
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *
  *Return: 0
+ *On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-  printf("Holberton\n");
+  return (write(1, &Holberton, 1);
 
 return 0;
 }
