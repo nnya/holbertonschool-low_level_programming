f2a90575d7216f4a7d1d57631425edbc1813143a
Nestor Nana <makak747@hotmail.com>
2018-06-27 00:37:13 +0000

    compute the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n = (n * (-1));
+	 return(n);
+	}
+	 else
+	return(n);
+ }
