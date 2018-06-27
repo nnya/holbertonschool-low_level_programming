30f34d1d81a1062626faf8796a84fb974d132d97
Nestor Nana <makak747@hotmail.com>
2018-06-27 07:11:24 +0000

    header file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  char naming[] = "Holberton";
-  int h;
-  for(h=0; h < 10; h++) 
-    {
-      _putchar(naming[h]);
-    }
+char naming[] = "Holberton";
+int h;
+for (h = 0; h < 10; h++)
+{
+_putchar(naming[h]);
+}
+_putchar(10);
+return (0);
+}
