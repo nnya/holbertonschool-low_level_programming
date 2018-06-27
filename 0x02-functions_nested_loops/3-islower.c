002311643791a7e3f3abdca0a6f525bab1dc8cc7
Nestor Nana <makak747@hotmail.com>
2018-06-27 06:51:00 +0000

    check lower cases

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _islower - check the code for Holberton School students.
+ * Description: check lower cases
+ * @c: starting point
  * Return: 0
  */
 
 int _islower(int c)
 {
-	if(c > 97 && c < 122)
-		return(1);
+	if (c > 97 && c < 122)
+		return (1);
 		else
-			return(0);
+			return (0);
 				}
