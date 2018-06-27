c295c6fd7a9b6cb45c619561f5c4e35f5946bd84
Nestor Nana <makak747@hotmail.com>
2018-06-27 06:42:40 +0000

    checking alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _isalpha - check the code for Holberton School students.
+ * Description: check the alphabet character
+ * @c: starting point
  * Return: Always 0.
  */
 
 int _isalpha(int c)
 {
-	if((c > 97 && c < 122) || (c > 67 && c < 90))
-			return(1);
-		return(0);
+	if ((c > 97 && c < 122) || (c > 67 && c < 90))
+		return (1);
+		return (0);
 }
