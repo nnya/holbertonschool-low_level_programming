55184ef9fecf5e99dbc0f743318436da43c091cc
Nestor Nana <makak747@hotmail.com>
2018-06-27 06:18:53 +0000

    printing the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,28 @@
 #include "holberton.h"
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
+* print_last_digit - check the code for Holberton School students.
+*@n : starting point
+*Description: printing last digit number.
+* Return:  0.
 */
 
 int print_last_digit(int n)
+
 {
 	int lastdigit;
-	if(n >= 0)
+
+if (n >= 0)
 {
 
-lastdigit = n%10;
+lastdigit = n % 10;
 _putchar(lastdigit + '0');
 return (lastdigit);
 }
 	else
-{ 
-	lastdigit = (n%10)*(-1);
+{
+	lastdigit = (n % 10) * (-1);
 _putchar(lastdigit + '0');
-return(lastdigit);
+return (lastdigit);
 }
 }
