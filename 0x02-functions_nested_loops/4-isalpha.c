3df7948012def119ee083779b25d0dcea41d79f7
Nestor Nana <makak747@hotmail.com>
2018-06-28 20:45:40 +0000

    UPdated 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+
+ * main - check the code for Holberton School students
+
+ *
+
+ * Return: 0
+
+ */
+
+int _islower(int c)
+
+{
+
+  if(c > 97 && c < 122)
+
+    return(1);
+}
+  else
+    {
+      if (C > 65 && C < 90)
+    return(0);
+    }
