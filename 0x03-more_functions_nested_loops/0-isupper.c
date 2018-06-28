a52f7028554dd3cbc796cba3bec82a297d4add17
Nestor Nana <makak747@hotmail.com>
2018-06-28 20:17:14 +0000

    selceting upper case letter

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 
-
 /**
-* main - check the code for Holberton School students.
+* _isupper - check the code for Holberton School students.
+* @c: character
 * Description: uppercase character
 * Return: Always 0.
 */
 
 int _isupper(int c)
 {
-  return (c >= 65 && c <= 90);
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
 }
