176791fe7ad04e48b7738bbf19acf00c1ff799fa
Nestor Nana <makak747@hotmail.com>
2018-06-28 19:50:57 +0000

    pushing header

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -1,12 +1,2 @@
-#include "holberton.h"
-/**
- * main - check the code for Holberton Students.
- * Description: creating a header file
- * Return: Always 0.
- */
-
-int main(void)
-{
-
-  return (0);
-}
+int _putchar(char c);
+int _isupper(int c);
