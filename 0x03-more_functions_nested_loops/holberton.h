d372eee1e10eb780ac0ae4a0184b2717a0fc3527
Nestor Nana <makak747@hotmail.com>
2018-06-28 18:16:21 +0000

    header file

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton Students.
+ * Description: creating a header file
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+  return (0);
+}
