a7ef6c00a31a1e90d0b86cebe5d5bd3ce06edc57
Nestor Nana <makak747@hotmail.com>
2018-06-28 21:50:48 +0000

    checking digital number

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _isdigit - is a digit.
+* @c: in
+* Description: digit numbers
+* Return: Always 0.
+*/
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+		else
+			return (0);
+				}
