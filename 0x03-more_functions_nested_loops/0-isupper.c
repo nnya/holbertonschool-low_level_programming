1a28ed5b7ee6710b6e1b8c0389e802bfef8223e4
Nestor Nana <makak747@hotmail.com>
2018-06-28 19:48:18 +0000

    checking upper case

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+
+/**
+* main - check the code for Holberton School students.
+* Description: uppercase character
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+  return (c >= 65 && c <= 90);
+}
