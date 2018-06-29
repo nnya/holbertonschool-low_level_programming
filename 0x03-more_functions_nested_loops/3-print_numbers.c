3a4932ee70ef857f7bd16d32540a96f5eef78888
Nestor Nana <makak747@hotmail.com>
2018-06-29 00:17:23 +0000

    printing numbers to 9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_numbers - is printing.
+* Description: multiplying variables
+* Return: Always 0.
+*/
+
+void print_numbers(void)
+
+{
+	int n;
+
+	for (n = '0';  n <= '9'; n++)
+
+	_putchar(n);
+	_putchar(10);
+}
