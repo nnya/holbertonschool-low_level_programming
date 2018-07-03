bbe0b6073c96bdfa1f3c7cb2c48b21f0eaa28c9d
Nestor Nana <makak747@hotmail.com>
2018-07-03 23:15:55 +0000

    return the length of a string.

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: the string
+ * Description: calculating the string length
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+
+int _strlen(char *s)
+
+{
+	len = strlen(*s);
+	return (0);
+}
