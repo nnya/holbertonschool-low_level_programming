6ae86985fe405e1f4babeab349750dbc7be4c092
Nestor Nana <makak747@hotmail.com>
2018-07-06 01:00:12 +0000

    print char out of 2 string

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 
 /**
- * swap_int - update the value
- * @number: character to print
- * Description: changing the value of a pointer
+ * puts2 - update the value
+ * @str: string
+ *Description: changing the value of a pointer
  * section header: prototype in holberton
- * Return: might be 0
  */
 void puts2(char *str)
 
@@ -14,7 +13,7 @@ void puts2(char *str)
 
 	for (number = 0; str[number] != '\0'; number++)
 	{
-		if(str[number] % 2 == 0)
+		if (str[number] % 2 == 0)
 			_putchar(str[number]);
 	}
 		_putchar('\n');
