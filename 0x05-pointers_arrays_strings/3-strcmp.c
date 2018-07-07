9424985874518ca74182e8b72ba687d9e3a3219d
Nestor Nana <makak747@hotmail.com>
2018-07-07 18:34:26 +0000

    compares two strings

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -2,6 +2,8 @@
 
 /**
  * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
  * Description: copie a string
  * section header: prototype in holberton
  * Return: might be 0
@@ -9,7 +11,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	
+
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		i++;
 
