f8cec6b76ee536f4dab974a15ddbc9eef41065fd
Nestor Nana <makak747@hotmail.com>
2018-07-07 18:31:17 +0000

    compares two strings

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compare two strings
+ * Description: copie a string
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+
+	return (s1[i] - s2[i]);
+}
