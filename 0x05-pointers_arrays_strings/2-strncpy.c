06ed0f54ed8fbb50e037851e7187a6335f81b1b2
Nestor Nana <makak747@hotmail.com>
2018-07-07 06:35:14 +0000

    copy a string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - update the value
+ * @dest: length of destination
+ * @src: length of source
+ * @n: length of destination
+ * Description: copie a string
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int srcl;
+
+	for (srcl = 0; srcl < n && src[srcl] != '\0'; srcl++)
+		dest[srcl] = src[srcl];
+	for (; src[srcl] < n; srcl++)
+		dest[srcl] = '\0';
+
+	return (dest);
+}
