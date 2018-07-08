9bede79faee47928b069f0b89ca438be796155df
Nestor Nana <makak747@hotmail.com>
2018-07-08 00:30:12 +0000

    capitalize all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - capitalize all words of a string
+ * @s: string
+ * Description: reverse the content of an array of integer
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		switch (s[i])
+		{
+		case 10: case ' ': case 44: case 59: case 46: case 33:
+		case 63: case 34: case 40: case 41: case 123: case 125: case 9:
+			break;
+		}
+		if ((s[i] >= 97) && (s[i] <= 122))
+			s[i] = (s[i] - 32);
+	return (s);
+}
