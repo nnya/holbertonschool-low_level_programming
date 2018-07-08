0bfcf217b1cad354b03b0963fa49e7c489abbd97
Nestor Nana <makak747@hotmail.com>
2018-07-08 03:27:21 +0000

    encode a string using rot13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *leet - update the value
+ * @s: string
+ * Description: reverse the content of an array of integer
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+char *rot13(char *s)
+
+{
+	int i, j;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			for (j = 0; input[j] != '\0'; j++)
+			{
+				if (s[i] == input[j])
+				{
+					s[i] = output[j];
+					break;
+				}
+			}
+		}
+		return (s);
+	}
