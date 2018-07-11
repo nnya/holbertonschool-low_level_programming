0ff740113277c139a677658db6f3a50225800fa6
Nestor Nana <makak747@hotmail.com>
2018-07-11 06:04:27 +0000

    string for any of a set of bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - string that searches bytes
+ * @accept: integer
+ * @s: integer
+ * Description: fill the memory with a constant byte
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+char *_strpbrk(char *s, char *accept)
+
+{
+ unsigned int j, i;
+
+ for (i = 0; s[i] != '\0'; i++)
+ {
+	for (j = 0; accept[j] != '\0'; j++)
+		if (s[i] == accept[j])
+			return (s + i);
+ }
+ return (0);
+}
