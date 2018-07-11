62248a4f7a64221b69b1102a5e182873477fd002
Nestor Nana <makak747@hotmail.com>
2018-07-11 20:06:51 +0000

    Cleaning github

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
 
 /**
  * print_array - return the length of a string
+ * @i: the i th element of the array
  * @n:number of elements of the array
  * @a: pointer to interger
  * Description: print the elements of an array of integers
@@ -13,10 +14,17 @@
 void print_array(int *a, int n)
 
 {
+	int i;
 
-for (*a = 0; *a < n; *a++)
-{
-printf("%d", *a);
-}
+	for (i = 0; i < n; i++)
+	{
+	if i == (n - 1)
+		{
+			printf("%d", *(a + i));
+		}
+	else
+	{
+		printf("%d", *(a + i));
+	}
 printf("\n");
 }
