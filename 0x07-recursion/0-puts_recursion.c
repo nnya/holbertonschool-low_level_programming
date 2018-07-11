d6c91bce559d5c82ed9150b4ae994844c586ff0f
Nestor Nana <makak747@hotmail.com>
2018-07-11 22:37:45 +0000

    printing a string

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * Description: fill the memory with a constant byte
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+void _puts_recursion(char *s)
+
+{
+	if (*s != '\0')
+{
+		_putchar(*s);
+	_puts_recursion(s + 1);
+}
+	else
+	_putchar('\n');
+
+}
