39a2c9f2feb7ce026d84ac7ae9aa5b4ea794b083
Nestor Nana <makak747@hotmail.com>
2018-07-11 06:08:24 +0000

    string for any of a set of bytes

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@
 char *_strpbrk(char *s, char *accept)
 
 {
- unsigned int j, i;
+unsigned int j, i;
 
- for (i = 0; s[i] != '\0'; i++)
- {
+for (i = 0; s[i] != '\0'; i++)
+{
 	for (j = 0; accept[j] != '\0'; j++)
 		if (s[i] == accept[j])
 			return (s + i);
- }
- return (0);
+}
+return (0);
 }
