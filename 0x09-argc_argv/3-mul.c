1c58c4ca3fe26acd26ee8689586b4f46fb58d9ec
Nestor Nana <makak747@hotmail.com>
2018-07-15 04:57:51 +0000

    multiplies two numbers

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 {
 
 	if (argc != 3)
+
 	{
 		printf("Error\n");
 		return (1);
@@ -22,9 +23,10 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		int i, j;
+
 		i = atoi(argv[1]);
 		j = atoi(argv[2]);
 		printf("%d\n",  i * j);
-  	}
+	}
 	return (0);
 }
