e12274ed3097499ca57695790426a05abd5675b2
Nestor Nana <makak747@hotmail.com>
2018-07-18 02:05:41 +0000

    return a pointer to a newly allocated space

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - function
+ * @str: factor
+ * Description: counter
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int len, i = 0;
+
+	len = 0;
+
+	if (str == 0)
+	{
+		return (0);
+	}
+	while (str[i] != '\0')
+		i++;
+	len = i;
+
+	array = malloc(sizeof(char) * len + 1);
+		i = 0;
+			while (str[i] != '\0')
+			{
+				array[i] = str[i];
+				i++;
+			}
+			array[i] = '\0';
+			return (array);
+}
