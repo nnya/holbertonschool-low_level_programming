620d18db87abd52012fa6cd4b5e99f329d6ee613
Nestor Nana <makak747@hotmail.com>
2018-07-19 23:09:04 +0000

    function that allocates memory using malloc.

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - function
+ * @ptr: factor
+ * @b: element
+ * Description: function that allocates memory using malloc
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+void *malloc_checked(unsigned int b)
+
+{
+	void *ptr = NULL;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+		return (ptr);
+}
