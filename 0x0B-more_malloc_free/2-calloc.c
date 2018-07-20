9c899330709cf23cdba53ed1d9fdafdb0aa2718d
Nestor Nana <makak747@hotmail.com>
2018-07-20 03:13:13 +0000

    allocate memory for an array

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ **_calloc - function
+ * @nmemb: element
+ * @size: element
+ * Description: function that concatenates two strings.
+ * section header: prototype in holberton
+ * Return: might be 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *array = NULL;
+
+	array = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(nmemb * size);
+
+	if (array == 0)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < (size * nmemb); i++)
+		array[i] = '\0';
+		return (array);
+}
