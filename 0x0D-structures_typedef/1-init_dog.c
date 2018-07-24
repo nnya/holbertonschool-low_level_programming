ff86f21741530760d3c46d62474d48d50689f65f
Nestor Nana <makak747@hotmail.com>
2018-07-24 04:57:54 +0000

    updated file

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -15,9 +14,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-	if (d == NULL)
+	if (!d)
+	{
 		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
