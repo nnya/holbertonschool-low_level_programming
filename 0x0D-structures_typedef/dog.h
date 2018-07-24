cdf6ff0333ea83b1678142056c165f776c356c03
Nestor Nana <makak747@hotmail.com>
2018-07-24 01:31:00 +0000

    new type struct dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_
+#define _dog_
+
+/**
+ * struct dog - Short description
+ * @name: name
+ * @owner: member
+ * @age: The age
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
