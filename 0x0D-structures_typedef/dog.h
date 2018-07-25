2b47a6426753d5a9b608300a6d36c44148c70aad
Nestor Nana <makak747@hotmail.com>
2018-07-25 01:20:33 +0000

    header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -19,4 +19,5 @@ dog_t;
 typedef struct dog dog_it;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
