89b13b2b304b0c16a248a2bf8d44aa31531e6ffd
Nestor Nana <makak747@hotmail.com>
2018-07-28 23:14:29 +0000

    priting string

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -29,7 +29,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			if (i + 1 < n)
 			{
-			printf("%s%s", string, separator);
+				if (separator)
+					printf("%s%s", string, separator);
+				else
+					printf("%s", string);
 			}
 			else
 			{
